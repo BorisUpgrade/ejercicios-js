46776a61900003b8240a2a15955d3415ac519fe3
borisUpgrade <boris.castro@bootcamp-upgrade.com>
2022-07-13 23:49:38 +0200

    Actualización enunciados

--- S1/1.variables/ex7.js
+++ S1/1.variables/ex7.js
@@ -1,3 +1,6 @@
+/*Dado el siguiente javascript, actualiza el valor de la variable ``globalBasePrice`` a 25000 
+actualiza la propiedad ``finalPrice`` de todos los coches con el valor de su propiedad ``basePrice`` 
+más el valor de la variable ``globalBasePrice``.*/
 let globalBasePrice = 25000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
--- S1/3.arrays/ex4.js
+++ S1/3.arrays/ex4.js
@@ -1,3 +1,4 @@
+/*Añade 2 elementos al array: "Morty" y "Summer". Muestra en consola el último personaje del array*/
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 
 rickAndMortyCharacters.push("Morty");
